fbc440b25830dcb1b98128417fc768be6d10fbe7
ditaa99 <dita_s@live.com>
2024-01-18 01:27:38 +0100

    update UI

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,70 +1,93 @@
-import { useState } from 'react';
-import axios from 'axios';
-import './index.css'
+import { useState } from "react";
+import axios from "axios";
+import "./index.css";
 
 const YourReactComponent = () => {
-    const [result, setResult] = useState('');
+  const [result, setResult] = useState("");
 
-    const handleClick = async () => {
-        try {
-            // Make a POST request to your Flask API
-            const response = await axios.post('http://localhost:5000/', {
-                // Pass any necessary data to your Python code
-                // For example, you might send user input
-                inputData: 'example data',
-            });
-            
-            console.log(response.data.result);
-            // Handle the response
-            setResult(response.data.result);
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+  const handleClick = async () => {
+    try {
+      // Make a POST request to your Flask API
+      const response = await axios.post("http://localhost:5000/", {
+        // Pass any necessary data to your Python code
+        // For example, you might send user input
+        inputData: "example data",
+      });
 
-    return (
+      console.log(response.data.result);
+      // Handle the response
+      setResult(response.data.result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-  <div className="App">
-    <div className="menu">
-      <button type="button" value="Save" className="menu-item">Save</button>
-      <button type="button" value="Import" className="menu-item">Import</button>         
-      <button type="button" value="Examples" className="menu-item">Examples</button>
-      <button type="button" value="Help" className="menu-item">Help</button>
-    </div>
+  return (
+    <div className="App">
+      <div className="menu">
+        <button type="button" value="Save" className="menu-item">
+          Save
+        </button>
+        <button type="button" value="Import" className="menu-item">
+          Import
+        </button>
+        <button type="button" value="Examples" className="menu-item">
+          Examples
+        </button>
+        <button type="button" value="Help" className="menu-item">
+          Help
+        </button>
+      </div>
 
-    <div className="symb_rules">
-    <div className="symbols">
-      <p>Terminal symbols: 
-        <input type="text"  style={{border: "none", borderBottom: "1px solid black"}} />
-      </p>
-      <p>Nonterminal symbols:  
-        <input type="text" style={{border: "none", borderBottom: "1px solid black"}} />
-      </p>
-      <p>Starting symbol: 
-        <input type="text"  style={{border: "none", borderBottom: "1px solid black", width:"30px"}} />
-      </p>
-    </div>
+      <div className="symb_rules">
+        <div className="symbols">
+          <div className="symbol-row">
+            <p className="label">Terminal symbols:</p>
+            <textarea
+              rows="1"
+              cols="5"
+              placeholder="Enter terminal symbols here"
+            ></textarea>
+          </div>
+          <div className="symbol-row">
+            <p className="label">Nonterminal symbols:</p>
+            <textarea
+              rows="1"
+              cols="5"
+              placeholder="Enter nonterminal symbols here"
+            ></textarea>
+          </div>
+          <div className="symbol-row">
+            <p className="label">Starting symbol:</p>
+            <textarea rows="1" cols="5" placeholder="S"></textarea>
+          </div>
+        </div>
 
-      <div className="rules">
-        <button>New</button>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus inventore sapiente doloribus deleniti suscipit sunt harum veritatis corrupti assumenda aliquid tenetur earum dolor mollitia, delectus dolore incidunt, neque ipsa eius.</p>
+        <div className="rules">
+        <button type="button" value="New" className="btn">
+          New
+        </button>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            inventore sapiente doloribus deleniti suscipit sunt harum veritatis
+            corrupti assumenda aliquid tenetur earum dolor mollitia, delectus
+            dolore incidunt, neque ipsa eius.
+          </p>
+        </div>
+      </div>
+      <div className="solution">
+        <p>Generate </p>
+        {/* <input type='text' id='generate' name='generate' /> */}
+      </div>
+      <div className="lang">
+        <p>Lanugage</p>
+      </div>
+      <div style={{ display: "none" }}>
+        <button onClick={handleClick}>Run Python Code</button>
+        <div>Result: {result}</div>
       </div>
     </div>
-    <div className="solution">
-      <p>Generate </p>
-      {/* <input type='text' id='generate' name='generate' /> */}
-    </div>
-    <div className="lang">
-      <p>Lanugage</p>
-    </div>
-    <div style={{ display: 'none' }}>
-        <button onClick={handleClick} >Run Python Code</button>
-        <div>Result: {result}</div>
-    </div>
-    </div>
-
-        
-    );
+  );
 };
 
 export default YourReactComponent;
